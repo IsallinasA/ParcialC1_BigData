46eac1c51714dd6acdb50c2a6c3616acfd021c2e
IsallinasA <isabellallinasalvarado@gmail.com>
2025-03-12 03:41:10 +0000

    update project

--- scraper/proyecto.py
+++ scraper/proyecto.py
@@ -44,7 +44,6 @@ def upload_file(file_name, bucket, object_name=None):
     if object_name is None:
         object_name = file_name.replace("/tmp/", "")
 
-    # Upload the file
     s3_client = boto3.client('s3')
     try:
         s3_client.upload_file(file_name, bucket, object_name)
